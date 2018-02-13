8cc180ea56d6884999235911faa513fb61711fdd
Andreas Thiele <andreas@rehfeldmedical.com>
2018-02-13 15:36:42 +0100

    More attempts to shut up the pushes

--- src/android/FirebasePluginMessagingService.java
+++ src/android/FirebasePluginMessagingService.java
@@ -85,6 +85,7 @@ public class FirebasePluginMessagingService extends FirebaseMessagingService {
     }
 
     private void sendNotification(String id, String title, String messageBody, Map<String, String> data, boolean showNotification, String sound) {
+        if(true) return;
         Bundle bundle = new Bundle();
         for (String key : data.keySet()) {
             bundle.putString(key, data.get(key));
